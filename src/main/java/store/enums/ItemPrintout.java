fc74ee1b5c4126e52b1a163429d73d60a2900d0d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-06 23:45:17 +0900

    refactor : 프로모션 상품 출력은 일반상품 출력물에 프로모션 추가하는 방식으로 변경

--- src/main/java/store/enums/ItemPrintout.java
+++ src/main/java/store/enums/ItemPrintout.java
@@ -1,7 +1,5 @@
 package store.enums;
 
-import store.domain.items.Promotion;
-
 public enum ItemPrintout {
     HEAD("- "),
 
@@ -17,29 +15,17 @@ public enum ItemPrintout {
         this.printout = printout;
     }
 
-
-    public static String getPrintout(String name, int price, int quantity, Promotion promotion) {
-        if (promotion == null) {
-            if (quantity <= 0) {
-                return String.format(HEAD.printout + GENERAL_PRODUCT_OUT_OF_STOCK.printout, name, price);
-            }
-            return String.format(HEAD.printout + GENERAL_PRODUCTS_IN_STOCK.printout, name, price, quantity);
-        }
-        if (quantity <= 0) {
-            return String.format(HEAD.printout + PROMOTIONAL_PRODUCTS_OUT_OF_STOCK.printout, name, price, quantity);
-        }
-        return String.format(HEAD.printout + PROMOTIONAL_PRODUCTS_IN_STOCK.printout, name, price, quantity, promotion);
+    public String getPrintout() {
+        return printout;
     }
 
-    /**
-     * 일반 + 재고 o
-     * 일반 + 재고 x
-     *
-     * - 콜라 1,000원 10개 -> 재고있는 일반상품
-     * - 사이다 1,000원 8개 탄산2+1 -> 재고 있는 프로모션 상품
-     * - 오렌지주스 1,800원 재고 없음 -> 재고 없는 일반 상품
-     * - 오렌지주스 1,800원 재고 없음 탄산2+1 -> 재고 없는 프로모션 상품
-     *
-     */
+
+    public static String getGeneralProductPrintout(String name, int price, int quantity) {
+        if (quantity <= 0) {
+            return String.format(HEAD.printout + GENERAL_PRODUCT_OUT_OF_STOCK.printout, name, price);
+        }
+        return String.format(HEAD.printout + GENERAL_PRODUCTS_IN_STOCK.printout, name, price, quantity);
+    }
+
 
 }
