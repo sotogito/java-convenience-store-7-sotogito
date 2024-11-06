9546b2cfc5a9a8e8cf4a19d14f8e699e211507e3
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-06 16:20:08 +0900

    feat : 상품 저장 order 생성

---
+++ src/main/java/store/domain/order/Order.java
@@ -0,0 +1,19 @@
+package store.domain.order;
+
+import store.domain.items.Product;
+
+public class Order {
+    private final Product product;
+    private final int purchaseQuantity;
+
+    public Order(Product product, int purchaseQuantity) {
+        this.product = product;
+        this.purchaseQuantity = purchaseQuantity;
+    }
+
+    public int getPurchaseAmountBasedOnQuantity() {
+        return purchaseQuantity * product.getPrice(); //fixme 상품 내부로 넘겨?
+    }
+
+}
+
