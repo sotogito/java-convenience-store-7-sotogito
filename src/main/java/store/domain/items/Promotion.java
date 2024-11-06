aafc5a99dafb6f23d0c8fdaf9990a7eef7a02360
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-06 14:34:14 +0900

    feat : Promotion 객체 생성

---
+++ src/main/java/store/domain/items/Promotion.java
@@ -0,0 +1,36 @@
+package store.domain.items;
+
+import java.time.LocalDateTime;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDateTime startDate;
+    private final LocalDateTime endDate;
+
+    public Promotion(String name, int buy, int get,
+                     LocalDateTime startDate, LocalDateTime endDate) {
+        this.buy = buy;
+        this.name = name;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean isSameName(String name) {
+        return this.name.equals(name);
+    }
+
+    /**
+     * LocalDateTime now = LocalDateTime.now(); System.out.println("getYear() = " + now.getYear());
+     * System.out.println("getMonth() = " + now.getMonth()); System.out.println("getDayOfMonth() = " +
+     * now.getDayOfMonth()); System.out.println("getDayOfWeek() = " + now.getDayOfWeek());
+     * System.out.println("getDayOfYear() = " + now.getDayOfYear());
+     */
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
