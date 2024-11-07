8dcacee0ca3829438d2b2f976006caeb2b5ce350
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-07 22:37:45 +0900

    fix : 프로모션 증정품 입력받을 시 출력물 변경

--- src/main/java/store/controller/ConvenienceStoreController.java
+++ src/main/java/store/controller/ConvenienceStoreController.java
@@ -11,13 +11,18 @@ import store.domain.order.Receipt;
 import store.domain.reader.ProductReader;
 import store.domain.reader.PromotionReader;
 import store.domain.record.OrderForm;
+import store.enums.AnswerWhether;
 import store.service.OrderService;
+import store.view.InputView;
+import store.view.OutputView;
 
 public class ConvenienceStoreController {
     /**
      * Conveni 출력에 필요한 객체 : Recipt
      */
     private OrderService orderService;
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     public void run() throws IOException {
         ConvenienceStoreroom storeroom = new ConvenienceStoreroom(new PromotionReader(), new ProductReader());
@@ -25,42 +30,20 @@ public class ConvenienceStoreController {
         Receipt receipt = new Receipt(cart);
         orderService = new OrderService(storeroom, receipt, cart);
 
+        System.out.println(storeroom);
+        processBuy();
+
+    }
+
+    private void processBuy() {
         while (orderService.isPurchase()) {
             tryBuy();
 
             if (orderService.isContainPromotionProduct()) { //todo 최소수량도 넘는지 봐야됨
                 //로직
                 if (orderService.isPromotionDate(DateTimes.now())) {
-
-                    /**
-                     * 1. 현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)
-                     * or
-                     * 1.현재 {상품명}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)
-                     */
-
-                    List<Order> ShortageStockPromotionOrders = orderService.getShortagePromotionalStock();
-                    if (!ShortageStockPromotionOrders.isEmpty()) {
-                        for (Order order : ShortageStockPromotionOrders) {
-                            String name = order.getProductName();
-                            int shortageQuantity = order.getNoPromotionQuantity();
-                            String answer = "인풋 값 넣어줘야함";
-
-                            orderService.processShortagePromotionalStock(answer, order, shortageQuantity);
-                        }
-                    }
-
-                    //note 부족한 수량 체울거냐 물어보고 업데이트 하는 로직
-                    List<Order> lackQuantityPromotionOrders = orderService.getLackQuantityPromotionOrders();
-                    if (!lackQuantityPromotionOrders.isEmpty()) {
-                        for (Order order : lackQuantityPromotionOrders) {
-                            String name = order.getProductName();
-                            int needAddQuantity = order.getNeedAddQuantity();
-                            String answer = "인풋 값 넣어줘야함";
-
-                            orderService.processLackQuantityPromotionOrders(answer, order, needAddQuantity);
-                        }
-                    }
-
+                    processShortageStockPromotionOrder();
+                    processLackQuantityPromotionOrder();
                 }
                 //일반으로 꼐산
             }
@@ -68,8 +51,58 @@ public class ConvenienceStoreController {
             //계속할거냐 물어보기
         }
         //영수증 출력
-
+        orderService.stopPurchase();
     }
+
+    //note 먼저
+    private void processShortageStockPromotionOrder() {
+        for (Order order : orderService.getShortagePromotionalStock()) {
+            String name = order.getProductName();
+            int shortageQuantity = order.getNoPromotionQuantity();
+            AnswerWhether answer = inputWhetherBuyNoPromotion(name, shortageQuantity);
+
+            orderService.processShortagePromotionalStock(answer, order, shortageQuantity);
+        }
+    }
+
+    private AnswerWhether inputWhetherBuyNoPromotion(String productName, int shortageQuantity) {
+        while (true) {
+            try {
+                String answer = inputView.inputWhether(
+                        String.format("현재 %s %,d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"
+                                , productName, shortageQuantity));
+
+                return AnswerWhether.valueOf(answer);
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
+    //note 나중에
+    private void processLackQuantityPromotionOrder() {
+        for (Order order : orderService.getLackQuantityPromotionOrders()) {
+            String name = order.getProductName();
+            int needAddQuantity = order.getNeedAddQuantity();
+            AnswerWhether answer = inputWhetherAddPromotionProduct(name, needAddQuantity);
+
+            orderService.processLackQuantityPromotionOrders(answer, order, needAddQuantity);
+        }
+    }
+
+    private AnswerWhether inputWhetherAddPromotionProduct(String productName, int needAddQuantity) {
+        while (true) {
+            try {
+                String answer = inputView.inputWhether(
+                        String.format("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
+                                , productName, needAddQuantity));
+                return AnswerWhether.valueOf(answer); //이거 꺼야함
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
+    }
+
 
     private void tryBuy() {
         while (true) {
@@ -83,7 +116,7 @@ public class ConvenienceStoreController {
     }
 
     private List<OrderForm> inputToOrderForm() {
-        return OrderParser.parse("[사이다-2],[감자칩-1]");
+        return OrderParser.parse(inputView.inputOrderProducts());
     }
 
 
