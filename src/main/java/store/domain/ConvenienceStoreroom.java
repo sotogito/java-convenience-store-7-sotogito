0f75acb69366a7062cdf5a338fde729d3e9e7bcd
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-09 01:22:33 +0900

    refactor : 구매 상품 찾는 인터패이스 implements

--- src/main/java/store/domain/ConvenienceStoreroom.java
+++ src/main/java/store/domain/ConvenienceStoreroom.java
@@ -1,7 +1,6 @@
 package store.domain;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import store.domain.items.Products;
 import store.domain.items.Promotions;
@@ -11,34 +10,27 @@ import store.domain.reader.ProductReader;
 import store.domain.reader.PromotionReader;
 import store.enums.ResourcePath;
 
-/**
- * 재고관리 해야될듯 0. 존재하는 상품인지 1. 상품 재고가 있는지 2. 일반상품인지 프로모션인지 3. 프로모션 기간인지 4. 알맞는 재고를 입력했는지
- * <p>
- * -> 이걸 순차적으로 만든게 서비스에
- */
-public class ConvenienceStoreroom {
+
+public class ConvenienceStoreroom implements OrderProductFinder {
     private final Products products;
     private final Promotions promotions;
 
     public ConvenienceStoreroom(PromotionReader promotionReader, ProductReader productReader) throws IOException {
         this.promotions = new Promotions(promotionReader.read(ResourcePath.PROMOTION.getPath()));
-        List<Product> products1 = productReader.read(ResourcePath.PRODUCT.getPath(), promotions);
-        for (Product product : products1) {
-            System.out.println(product.getName());
-        }
-
-        this.products = new Products(products1);
+        this.products = new Products(productReader.read(ResourcePath.PRODUCT.getPath(), promotions));
     }
 
+    @Override
     public Product findProductByNameAndQuantity(String name, int quantity) {
-        return products.getProductByName(name, quantity);
+        return products.getProductByNameAndQuantity(name, quantity);
     }
 
+    @Override
     public Product findGeneralProductByNameAndQuantity(String name, int quantity) {
-        return products.getGeneralProduct(name, quantity);
+        return products.getGeneralProductByNameAndQuantity(name, quantity);
     }
 
-    public Product findGeneralProductByName(String name) {
+    private Product findGeneralProductByName(String name) {
         return products.getGeneralProductByName(name);
     }
 
@@ -67,6 +59,10 @@ public class ConvenienceStoreroom {
             Product generalProduct = findGeneralProductByName(product.getName());
             generalProduct.decreaseQuantitySingly();
         }
+    }
+
+    public boolean isSufficientStockAfterGetPromotionProduct(String name, int afterGetPromotionQuantity) {
+        return products.isSufficientStockByNameAndQuantity(name, afterGetPromotionQuantity);
     }
 
 
