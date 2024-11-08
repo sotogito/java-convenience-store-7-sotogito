0288e5c08021697b2655c4f44edb61ec038d644d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-08 21:41:08 +0900

    refactor : readLine input에서 예외 잡기

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,18 +1,28 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class InputView {
 
-
     public String inputOrderProducts() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
-        return Console.readLine();
+
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException | IllegalStateException e) {
+            throw e;
+        }
     }
 
     public String inputWhether(String message) {
         System.out.println(message);
-        return Console.readLine();
+
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException | IllegalStateException e) {
+            throw e;
+        }
     }
 
 }
