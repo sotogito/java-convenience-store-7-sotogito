53ecb7750b666693a1deb556442457702beaef36
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-10 01:30:46 +0900

    refactor : 주문 입력 규칙 Enum으로 정의

---
+++ src/main/java/store/enums/OrderInputForm.java
@@ -0,0 +1,19 @@
+package store.enums;
+
+public enum OrderInputForm {
+    OPENER("["),
+    CLOSER("]"),
+    NAME_QUANTITY_DELIMITER("-"),
+    ORDER_DELIMITER(",");
+
+    private final String value;
+
+    OrderInputForm(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value;
+    }
+
+}
