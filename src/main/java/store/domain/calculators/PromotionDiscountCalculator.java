234051c17cddc5a11faae61406dfe5dccdceaed0
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-11 18:26:48 +0900

    feat : 프로모션 기간에 있는 프로모션 상품만 할인 적용

--- src/main/java/store/domain/calculators/PromotionDiscountCalculator.java
+++ src/main/java/store/domain/calculators/PromotionDiscountCalculator.java
@@ -1,18 +1,25 @@
 package store.domain.calculators;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
 import java.util.Map;
 import store.domain.items.item.Product;
+import store.domain.items.item.PromotionProduct;
 import store.domain.order.Cart;
 
 public class PromotionDiscountCalculator implements DiscountCalculator {
 
     @Override
     public int calculate(Cart cart) {
+        LocalDateTime nowDate = DateTimes.now();
         int totalPromotions = 0;
         for (Map.Entry<Product, Integer> entry : cart.getOrderPromotionProducts().entrySet()) {
-            int productPrice = entry.getKey().getPrice();
-            int count = entry.getValue();
-            totalPromotions += calculateAmountOfOne(productPrice, count);
+            PromotionProduct product = (PromotionProduct) entry.getKey();
+            if (product.isValidDate(nowDate)) {
+                int productPrice = entry.getKey().getPrice();
+                int count = entry.getValue();
+                totalPromotions += calculateAmountOfOne(productPrice, count);
+            }
         }
         return totalPromotions;
     }
