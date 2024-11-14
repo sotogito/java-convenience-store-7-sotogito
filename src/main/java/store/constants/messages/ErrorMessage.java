935519d158a7296f5d7d4341baf242bd4f9ec5a3
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-14 18:06:04 +0900

    feat : 존재하지 않는 상품 예외 메시지 상수 추가

--- src/main/java/store/constants/messages/ErrorMessage.java
+++ src/main/java/store/constants/messages/ErrorMessage.java
@@ -3,6 +3,7 @@ package store.constants.messages;
 public enum ErrorMessage {
     OUT_OF_STOCK("재고 부족 오류"),
     NOT_EXIST_PROMOTION("존재하지 않는 프로모션"),
+    NOT_EXIST_PRODUCT("존재하지 않는 상품"),
 
     INPUT_INCORRECT_ORDER_FORM("올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
 
