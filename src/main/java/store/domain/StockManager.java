a14221fa7fd2aa269ba5411e180bb462c4a25438
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-14 17:49:34 +0900

    fix : 프로모션 기간이 아닌 프로모션 상품 선 일반상품 후 프로모션 재고 차감

--- src/main/java/store/domain/StockManager.java
+++ src/main/java/store/domain/StockManager.java
@@ -1,8 +1,6 @@
 package store.domain;
 
-import java.util.List;
 import java.util.Map;
-import store.constants.messages.ErrorMessage;
 import store.domain.items.Products;
 import store.domain.items.item.Product;
 import store.domain.items.item.PromotionProduct;
@@ -13,56 +11,53 @@ public class StockManager {
         for (Map.Entry<Product, Integer> entry : productQuantity.entrySet()) {
             Product product = entry.getKey();
             Integer quantity = entry.getValue();
-            validateStock(products, product, quantity);
-            precessDecrease(products, product, quantity);
+
+            handePromotionType(products, product, quantity);
         }
     }
 
-    private void validateStock(Products products, Product orderProduct, int orderQuantity) {
-        int totalStick = 0;
-        List<Product> sameNameProduct = products.getSameNameProducts(orderProduct.getName());
-        for (Product product : sameNameProduct) {
-            totalStick += product.getQuantity();
-        }
-        if (totalStick < orderQuantity) {
-            throw new IllegalArgumentException(ErrorMessage.INPUT_INSUFFICIENT_STOCK_ORDER.get());
-        }
-    }
 
-    private void precessDecrease(Products products, Product product, Integer quantity) {
-        if (product instanceof PromotionProduct promotionProduct) {
-            promotionProductDecrease(products, product, quantity, promotionProduct);
+    private void handePromotionType(Products products, Product product, int quantity) {
+        if (product instanceof PromotionProduct) {
+            decreasePromotionStock(products, product, quantity);
             return;
         }
-        generalProductDecrease(product, quantity);
+        decreaseGeneralStock(products, product, quantity);
     }
 
-    private void generalProductDecrease(Product product, Integer quantity) {
-        product.decreaseQuantity(quantity);
-    }
+    private void decreaseGeneralStock(Products products, Product product, int quantity) {
+        int remainingQuantity = 0;
 
-    private void promotionProductDecrease(Products products, Product product, Integer quantity,
-                                          PromotionProduct promotionProduct) {
         for (int i = 0; i < quantity; i++) {
-            if (changeOutOfStockProductToGeneral(products, product, promotionProduct)) {
+            if (!product.isOutOfStock()) {
+                product.decreaseQuantitySingly();
                 continue;
             }
-            promotionProduct.decreaseQuantitySingly();
+            remainingQuantity = quantity - i;
+            break;
         }
-    }
-
-    private boolean changeOutOfStockProductToGeneral(Products products, Product product,
-                                                     PromotionProduct promotionProduct) {
-        if (promotionProduct.isOutOfStock()) {
-            Product generalProduct = findGeneralProductByName(products, product.getName());
-            generalProduct.decreaseQuantitySingly();
-            return true;
+        if (remainingQuantity == 0) {
+            return;
         }
-        return false;
+        Product promotionProduct = products.findPromotionProductByName(product.getName());
+        promotionProduct.decreaseQuantity(remainingQuantity);
     }
 
-    private Product findGeneralProductByName(Products products, String name) {
-        return products.getGeneralProductByName(name);
-    }
+    private void decreasePromotionStock(Products products, Product product, int quantity) {
+        int remainingQuantity = 0;
 
+        for (int i = 0; i < quantity; i++) {
+            if (!product.isOutOfStock()) {
+                product.decreaseQuantity(quantity);
+                continue;
+            }
+            remainingQuantity = quantity - i;
+            break;
+        }
+        if (remainingQuantity == 0) {
+            return;
+        }
+        Product generalProduct = products.findGeneralProductByName(product.getName());
+        generalProduct.decreaseQuantity(remainingQuantity);
+    }
 }
